0cf1503e53733a761bb3d3e3b55b17beb1190043
Emmanuel Boutin <emmanuel.boutin@jabberbees.com>
2018-06-13 13:43:07 +0200

    Cleaned up erlang.mk

--- plugins.mk
+++ plugins.mk
@@ -1,22 +1,29 @@
 feature_verbose_0 = @echo " FEATURE " $(filter %.feature,$(?F));
 feature_verbose = $(feature_verbose_$(V))
 
-# Core targets.
-
-define compile_feature
+define compile_features
 	$(verbose) mkdir -p ebin/ $(TEST_DIR)
-	$(feature_verbose) $(call erlang,$(call compile_feature.erl,$(1)))
+	$(feature_verbose) $(call erlang,$(call compile_features.erl,$(1)))
 endef
 
-define compile_feature.erl
+define compile_features.erl
 	[begin
-		ecucumber_ct:generate_source(F,
-			[{output_src_dir, "$(call core_native_path,$(TEST_DIR))"}])
+		case ecucumber_ct:generate_source(F,
+			[{output_src_dir, "$(call core_native_path,$(TEST_DIR))"}]) of
+		{ok, Filename} ->
+			io:format("          -> ~s~n", [Filename]);
+		{error, Reason} ->
+			io:format("          error: ~p~n", [Reason]);
+		{failed, Line, Reason} ->
+			io:format("          error: ~p, line ~p~n", [Reason, Line])
+		end
 	end || F <- string:tokens("$(1)", " ")],
 	halt().
 endef
 
 ifneq ($(wildcard features/),)
-ebin/$(PROJECT).app:: $(sort $(call core_find,features/,*.feature))
-	$(if $(strip $?),$(call compile_feature,$?))
+ecucumber-features: $(sort $(call core_find,features/,*.feature))
+	$(if $(strip $?),$(call compile_features,$?))
+
+ebin/$(PROJECT).app:: test-deps ecucumber-features
 endif
